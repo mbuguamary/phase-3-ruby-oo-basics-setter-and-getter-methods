21ce9dbc5b8a249b1893f9f02ed444a63c0a72c0
mbuguamary <mary.wairimu01@student.moringaschool.com>
2022-10-20 13:35:57 +0300

    completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
-
+def name=(name)
+    @name=name
 end
+def name
+    @name
+end 
+def breed=(breed)
+    @breed=breed
+
+end 
+def breed
+    @breed
+end
+end 
+kanye =Dog.new
+kanye.name='Kanye'
+puts kanye.name
+kanye.breed='Mastif'
+puts kanye.breed
